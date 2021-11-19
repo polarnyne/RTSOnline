3c8036e4ec491a1d1a3e270df549da0a7059d317
Polarnyne <davidgfunes@gmail.com>
2021-11-19 20:52:49 -0300

    Select multiple units handler

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -6,17 +7,57 @@ using UnityEngine.Events;
 
 public class Unit : NetworkBehaviour
 {
-    [SerializeField] private UnityEvent onSelected = null;
-    [SerializeField] private UnityEvent onDeselected = null;
+    [SerializeField] private UnitMovement unitMovement  = null;
+    [SerializeField] private UnityEvent onSelected      = null;
+    [SerializeField] private UnityEvent onDeselected    = null;
+
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    public UnitMovement GetUnitMovement() 
+    {
+        return unitMovement;
+    }
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 
     #region Client
+
+    public override void OnStartClient()
+    {
+        if(!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if(!isClientOnly || !hasAuthority) { return; }
+        
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
 
     [Client]
     public void Select() 
     {
         if (!hasAuthority) { return; }
 
-        onSelected ? . Invoke();
+        onSelected?.Invoke();
     }
 
     [Client]
@@ -24,7 +65,7 @@ public class Unit : NetworkBehaviour
     {
         if (!hasAuthority) { return; }
 
-        onDeselected ? . Invoke();
+        onDeselected?.Invoke();
     }
 
     #endregion
--- Assets/Scripts/Units/UnitCommandGiver.cs
+++ Assets/Scripts/Units/UnitCommandGiver.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UnitCommandGiver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private UnitSelectionHandler unitSelectionHandler = null;
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+
+    private Camera mainCamera;
+
+    private void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(!Mouse.current.rightButton.wasPressedThisFrame) { return; }
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
         
+        TryMove(hit.point);
+    }
+
+    private void TryMove(Vector3 point) 
+    {
+        foreach(Unit unit in unitSelectionHandler.SelectedUnits)
+        {
+            unit.GetUnitMovement().CmdMove(point);
+        }
     }
 }
